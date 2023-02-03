226d4ad80fe5c9df695e539d12cc372f557f1082
SeanAllen19 <saallen19.dev@gmail.com>
2023-02-03 16:06:13 -0600

    finally adding my code to Github

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const termData = require('./db/db.json');
+const PORT = 3001;
+const fs = require('fs');
+const { json } = require('express');
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+
+  })
+});
+
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+    
+      let tempData = JSON.parse(data);
+      let newNote = { 
+        title: req.body.title,
+        text: req.body.text
+      }
+      tempData.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(tempData), err=> {
+      if (err) throw err;
+      res.redirect('/notes')
+    })
+  })
+});
+
+app.delete('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+    
+      let tempData = JSON.parse(data);
+      let newNote = { 
+        title: req.body.title,
+        text: req.body.text
+      }
+      tempData.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(tempData), err=> {
+      if (err) throw err;
+      res.redirect('/notes')
+    })
+  })
+});
+
+//HTML ROUTES
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+
